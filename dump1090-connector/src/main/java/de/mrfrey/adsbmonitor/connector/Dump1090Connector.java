5795c63fbdf6b00ce1ff1309efde0e5b9de3212a
Rainer Frey <frey.rainer@gmail.com>
2016-03-21 08:15:16 +0100

    Initial Project Commit

---
+++ dump1090-connector/src/main/java/de/mrfrey/adsbmonitor/connector/Dump1090Connector.java
@@ -0,0 +1,12 @@
+package de.mrfrey.adsbmonitor.connector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dump1090Connector {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dump1090Connector.class, args);
+	}
+}
---
+++ ui-backend/src/main/groovy/de/mrfrey/adsbmonitor/connector/UiBackendApplication.groovy
@@ -0,0 +1,12 @@
+package de.mrfrey.adsbmonitor.connector
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class UiBackendApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run UiBackendApplication, args
+	}
+}
